1b719187d5e94739dd4105164bc8a07d032cd682
xerox <xerox@back.engineering>
2020-10-03 01:24:07 -0700

    added efi bundler...

---
+++ Efi Bundler/bundler.cpp
@@ -0,0 +1,92 @@
+#include "bundler.h"
+
+namespace bundler
+{
+	std::pair<std::uint32_t, std::uint32_t> add_section(std::vector<std::uint8_t>& image, const char* name, std::size_t size, std::uint32_t protect)
+	{
+		auto align = [](std::uint32_t size, std::uint32_t align, std::uint32_t addr) -> std::uint32_t
+		{
+			if (!(size % align))
+				return addr + size;
+			return addr + (size / align + 1) * align;
+		};
+
+		auto section_header = reinterpret_cast<PIMAGE_SECTION_HEADER>(
+			((std::uint64_t)&NT_HEADER(image.data())->OptionalHeader) +
+				NT_HEADER(image.data())->FileHeader.SizeOfOptionalHeader);
+
+		auto new_section = &section_header[NT_HEADER(image.data())->FileHeader.NumberOfSections];
+		memset(new_section, NULL, sizeof(IMAGE_SECTION_HEADER));
+		memcpy(new_section->Name, name, 8);
+
+		new_section->Characteristics = protect;
+		section_header[NT_HEADER(image.data())->FileHeader.NumberOfSections].Misc.VirtualSize =
+			align(size, NT_HEADER(image.data())->OptionalHeader.SectionAlignment, NULL);
+
+		new_section->VirtualAddress = align(section_header[
+			NT_HEADER(image.data())->FileHeader.NumberOfSections - 1].Misc.VirtualSize,
+				NT_HEADER(image.data())->OptionalHeader.SectionAlignment, section_header[
+					NT_HEADER(image.data())->FileHeader.NumberOfSections - 1].VirtualAddress);
+
+		new_section->SizeOfRawData =
+			align(size, NT_HEADER(image.data())->OptionalHeader.FileAlignment, 0);
+
+		new_section->PointerToRawData =
+			align(section_header[NT_HEADER(image.data())->FileHeader.NumberOfSections - 1].SizeOfRawData,
+				NT_HEADER(image.data())->OptionalHeader.FileAlignment, section_header[NT_HEADER(image.data())->
+					FileHeader.NumberOfSections - 1].PointerToRawData);
+
+		NT_HEADER(image.data())->OptionalHeader.SizeOfImage = section_header[
+			NT_HEADER(image.data())->FileHeader.NumberOfSections].VirtualAddress +
+				section_header[NT_HEADER(image.data())->FileHeader.NumberOfSections].Misc.VirtualSize;
+
+		++NT_HEADER(image.data())->FileHeader.NumberOfSections;
+		auto raw_data_rva = new_section->PointerToRawData;
+		auto virt_data_rva = new_section->VirtualAddress;
+		auto raw_data_size = new_section->SizeOfRawData;
+
+		image.resize(raw_data_rva + raw_data_size);
+		memset(image.data() + raw_data_rva, NULL, raw_data_size);
+		return { raw_data_rva, virt_data_rva };
+	}
+
+	// module_base is .efi section base in this case...
+	std::uint32_t map_module(std::uint8_t* module_base, std::vector<std::uint8_t>& map_from)
+	{
+		// copy nt headers...
+		memcpy(module_base, map_from.data(), NT_HEADER(map_from.data())->OptionalHeader.SizeOfHeaders);
+		auto sections = reinterpret_cast<PIMAGE_SECTION_HEADER>(
+			(std::uint8_t*)&NT_HEADER(map_from.data())->OptionalHeader +
+				NT_HEADER(map_from.data())->FileHeader.SizeOfOptionalHeader);
+
+		// copy sections...
+		for (auto i = 0u; i < NT_HEADER(map_from.data())->FileHeader.NumberOfSections; ++i)
+		{
+			auto section = &sections[i];
+			memcpy(module_base + section->VirtualAddress, map_from.data() + section->PointerToRawData, section->SizeOfRawData);
+		}
+
+		return NT_HEADER(map_from.data())->OptionalHeader.AddressOfEntryPoint;
+	}
+
+	void bundle(std::vector<std::uint8_t>& bundle_into, std::vector<std::uint8_t>& bundle_module)
+	{
+		auto [trp_section_disk, trp_section_virt] = add_section(bundle_into, ".trp", sizeof shellcode::stub, SECTION_RWX);
+		auto [mod_section_disk, mod_section_virt] = add_section(bundle_into, ".efi", bundle_module.size(), SECTION_RWX);
+		bundler::map_module(bundle_into.data() + mod_section_disk, bundle_module);
+
+		std::printf("[+] added .trp section at rva -> 0x%x, size -> 0x%x\n", trp_section_virt, sizeof shellcode::stub);
+		std::printf("[+] added .efi section at rva -> 0x%x, size -> 0x%x\n", mod_section_virt, bundle_module.size());
+
+		// setup stub shellcode...
+		*reinterpret_cast<std::int32_t*>(&shellcode::stub[25]) = mod_section_virt - trp_section_virt;
+		*reinterpret_cast<std::int32_t*>(&shellcode::stub[45]) = trp_section_virt;
+		*reinterpret_cast<std::int32_t*>(&shellcode::stub[75]) = NT_HEADER(bundle_into.data())->OptionalHeader.AddressOfEntryPoint;
+		memcpy(bundle_into.data() + trp_section_disk, shellcode::stub, sizeof shellcode::stub);
+		std::printf("[+] added stub code to .trp section...\n");
+
+		// set entry point to .trp section...
+		NT_HEADER(bundle_into.data())->OptionalHeader.AddressOfEntryPoint = trp_section_virt;
+		std::printf("[+] changed base modules entry point to -> (.trp section base) 0x%x\n", trp_section_virt);
+	}
+}
---
+++ Efi Bundler/shellcode.cpp
@@ -0,0 +1,57 @@
+#include "shellcode.h"
+
+namespace shellcode
+{
+	void* entry_stub(void* a, void* b)
+	{
+		// 0xDEADBEEF is replaced at runtime...
+		auto module_base = reinterpret_cast<std::uintptr_t>(&entry_stub) + 0xDEADBEEF;
+		auto bootmgfw_base = reinterpret_cast<std::uintptr_t>(&entry_stub) - 0xDEADBEEF;
+		NT_HEADER(bootmgfw_base)->OptionalHeader.AddressOfEntryPoint = 0xDEADBEEF;
+
+		// fix relocs of the module in .efi section...
+		auto base_reloc_dir = &NT_HEADER(module_base)->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+		if (base_reloc_dir->VirtualAddress)
+		{
+			auto reloc = reinterpret_cast<PIMAGE_BASE_RELOCATION>(module_base + base_reloc_dir->VirtualAddress);
+			for (auto current_size = 0u; current_size < base_reloc_dir->Size; )
+			{
+				std::uint32_t reloc_count = (reloc->SizeOfBlock - sizeof(IMAGE_BASE_RELOCATION)) / sizeof(std::uint16_t);
+				auto reloc_data = reinterpret_cast<std::uint16_t*>((std::uint8_t*)reloc + sizeof(IMAGE_BASE_RELOCATION));
+				auto reloc_base = reinterpret_cast<std::uint8_t*>(module_base) + reloc->VirtualAddress;
+
+				for (auto i = 0u; i < reloc_count; ++i, ++reloc_data)
+				{
+					std::uint16_t data = *reloc_data;
+					std::uint16_t type = data >> 12;
+					std::uint16_t offset = data & 0xFFF;
+
+					switch (type)
+					{
+					case IMAGE_REL_BASED_ABSOLUTE:
+						break;
+					case IMAGE_REL_BASED_DIR64:
+					{
+						auto rva = reinterpret_cast<std::uintptr_t*>(reloc_base + offset);
+						*rva = module_base + (*rva - NT_HEADER(module_base)->OptionalHeader.ImageBase);
+						break;
+					}
+					default:
+						break;
+					}
+				}
+
+				current_size += reloc->SizeOfBlock;
+				reloc = reinterpret_cast<PIMAGE_BASE_RELOCATION>(reloc_data);
+			}
+		}
+
+		// call our entry...
+		reinterpret_cast<void(__fastcall*)(void*, void*)>(
+			module_base + NT_HEADER(module_base)->OptionalHeader.AddressOfEntryPoint)(a, b);
+
+		// call the original entry...
+		return reinterpret_cast<void* (__fastcall*)(void*, void*)>(
+			bootmgfw_base + NT_HEADER(bootmgfw_base)->OptionalHeader.AddressOfEntryPoint)(a, b);
+	}
+}
